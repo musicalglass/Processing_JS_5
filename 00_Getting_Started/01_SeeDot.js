51e01ceb71d8e9c00cdfe1152c02062edcae163a
DillingerLee <musicalglass@hotmail.com>
2026-01-13 06:48:42 -0600

    Add files via upload

---
+++ 00_Getting_Started/01_SeeDot.js
@@ -0,0 +1,9 @@
+function setup() {
+	describe('Make a point with fat stroke weight.');
+	createCanvas(400, 400);
+
+	background(255);    // Clear the Screen White
+	stroke(255, 0, 0);  // Set the Color
+	strokeWeight(18); 	// Set the Width
+	point(200, 200); 	// Make your Point!
+}
